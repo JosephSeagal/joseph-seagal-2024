1f026cd7a164e93eedce532974183b265cff9107
JosephSeagal <58431081+JosephSeagal@users.noreply.github.com>
2024-02-28 00:07:49 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "https://josephseagal.com/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Joseph Seagal",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
